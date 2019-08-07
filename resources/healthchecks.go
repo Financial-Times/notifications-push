0d13f8fa9f3af144b9bd2a9ade35724e056a2552
Tsvetan Dimitrov <tsvetan.dimitrov@ft.com>
2019-08-07 18:52:58 +0300

    Added helm configuration and improved healthcheck

--- resources/healthchecks.go
+++ resources/healthchecks.go
@@ -1,6 +1,7 @@
 package resources
 
 import (
+	"bytes"
 	"net/http"
 	"time"
 
@@ -13,16 +14,22 @@ import (
 
 type HealthCheck struct {
 	Consumer kafka.Consumer
+	ApiGatewayGTGAddress string
 }
 
-func NewHealthCheck(kafkaConsumer kafka.Consumer) *HealthCheck {
+func NewHealthCheck(kafkaConsumer kafka.Consumer, apiGatewayGTGAddress string) *HealthCheck {
 	return &HealthCheck{
 		Consumer: kafkaConsumer,
+		ApiGatewayGTGAddress: apiGatewayGTGAddress,
 	}
 }
 
 func (h *HealthCheck) Health() func(w http.ResponseWriter, r *http.Request) {
-	checks := []fthealth.Check{h.queueCheck()}
+
+	var checks []fthealth.Check
+	checks = append(checks, h.queueCheck())
+	checks = append(checks, h.apiGatewayCheck())
+
 	hc := fthealth.TimedHealthCheck{
 		HealthCheck: fthealth.HealthCheck{
 			SystemCode:  "upp-notifications-push",
@@ -52,6 +59,11 @@ func (h *HealthCheck) GTG() gtg.Status {
 	if _, err := h.checkAggregateMessageQueueReachable(); err != nil {
 		return gtg.Status{GoodToGo: false, Message: err.Error()}
 	}
+
+	if _, err := h.checkApiGatewayService(); err != nil {
+		return gtg.Status{GoodToGo: false, Message: err.Error()}
+	}
+
 	return gtg.Status{GoodToGo: true}
 }
 
@@ -63,4 +75,40 @@ func (h *HealthCheck) checkAggregateMessageQueueReachable() (string, error) {
 	}
 
 	return "Error connecting to kafka", errors.New("Error connecting to kafka queue")
+}
+
+// checks if apiGateway service is available
+func (h *HealthCheck) apiGatewayCheck() fthealth.Check {
+	return fthealth.Check{
+		ID:               "api-gateway-check",
+		Name:             "ApiGatewayCheck",
+		Severity:         1,
+		BusinessImpact:   "If apiGateway service is not available, consumer's helthcheck will return false ",
+		TechnicalSummary: "Checking if apiGateway service is available or not",
+		PanicGuide:       "https://dewey.ft.com/upp-notifications-push.html",
+		Checker:          h.checkApiGatewayService,
+	}
+}
+
+
+func (h *HealthCheck) checkApiGatewayService() (string, error) {
+
+	r, err := http.NewRequest("GET", h.ApiGatewayGTGAddress, bytes.NewReader([]byte("")))
+	if err != nil {
+		return "", errors.New("Error creating request")
+	}
+
+	httpClient := &http.Client{Timeout: time.Second * 15}
+	res, err := httpClient.Do(r)
+	if err != nil {
+		return "", errors.New("Error making http request to GTG endpoint")
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusOK || res.StatusCode == http.StatusForbidden {
+		return "ApiGateway service is working", nil
+	}
+
+	return "", errors.New("Unable to verify ApiGateway service is working")
+
 }
